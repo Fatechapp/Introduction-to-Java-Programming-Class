edb0ad02459c5e042189c9f80490d35bf7ab814d
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2023-01-24 22:18:32 +0700

    Add files via upload

---
+++ java print and println/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        print("Hello" + "World");
+    }
+    public static void print(int i) {
+        System.out.print(i);
+    }public static void print(float f) {
+        System.out.print(f);
+    }public static void print(double d) {
+        System.out.print(d);
+    }public static void print(char c) {
+        System.out.print(c);
+    }public static void print(char[] c) {
+        System.out.print(c);
+    }public static void print(String s) {
+        System.out.print(s);
+    }public static void print(long l) {
+        System.out.print(l);
+    }public static void print(Object obj) {
+        System.out.print(obj);
+    }public static void print(boolean boo) {
+        System.out.print(boo);
+    }
+}
