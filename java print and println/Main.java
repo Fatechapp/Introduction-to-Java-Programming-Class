593c655db56b8e4f3acb22c1da699e7d07dc63ff
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2023-01-24 22:19:38 +0700

    Update Main.java

--- java print and println/Main.java
+++ java print and println/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        print("Hello" + "World");
+        print("Print Hello" + " World");
+        println("Println Hello" + " World");
     }
     public static void print(int i) {
         System.out.print(i);
@@ -20,5 +21,25 @@ public class Main {
         System.out.print(obj);
     }public static void print(boolean boo) {
         System.out.print(boo);
+    }
+
+    public static void println(int i) {
+        System.out.println(i);
+    }public static void println(float f) {
+        System.out.println(f);
+    }public static void println(double d) {
+        System.out.println(d);
+    }public static void println(char c) {
+        System.out.println(c);
+    }public static void println(char[] c) {
+        System.out.println(c);
+    }public static void println(String s) {
+        System.out.println(s);
+    }public static void println(long l) {
+        System.out.println(l);
+    }public static void println(Object obj) {
+        System.out.println(obj);
+    }public static void println(boolean boo) {
+        System.out.println(boo);
     }
 }
