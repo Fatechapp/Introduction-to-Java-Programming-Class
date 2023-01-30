ee801be18f3dd2588bd3b2768ad9395868d3ef4a
Fatecha Rizqi Putra Prasetyo <74399211+Fatechapp@users.noreply.github.com>
2023-01-30 21:11:52 +0700

    Update OOP.java

--- Latihan dan Review sebelum UAS sem 1/OOP/OOP.java
+++ Latihan dan Review sebelum UAS sem 1/OOP/OOP.java
@@ -2,6 +2,30 @@ package OOP;
 
 public class OOP {
     public static void main(String[] args) {
-        
+        /*OOP adalah singkatan dari "Object Oriented Programming". Ini adalah paradigma pemrograman
+         yang menekankan pada konsep objek sebagai entitas utama untuk membangun program.
+
+         Keuntungan dalam menggunakan OOP:
+         1. Modularitas: Membuat kode yang terstruktur dan mudah dipelihara.
+         2. Reusability: Kode dapat diterapkan kembali pada proyek lain.
+         3. Abstraction: Membuat program lebih mudah dipahami dan dipelihara.
+         4. encapsulation: Membuat kode lebih aman dan terlindungi dari pengaruh luar.
+         5. inheritance: Membuat proses pengembangan kode lebih cepat dan efisien.
+         6. Polymorphism: Membuat kode lebih fleksibel dan mudah untuk diubah dan dikembangkan.
+
+         Access Modifiers adalah konsep dalam OOP yang menentukan tingkat akses suatu properti atau
+         metode dalam sebuah objek. Berikut adalah beberapa jenis akses modifier dalam OOP:
+         1. Public: Dapat diakses dari mana saja dalam program.
+         2. Private: Hanya dapat diakses dalam objek yang mengandung properti atau metode tersebut.
+         3. Protected: Dapat diakses dalam objek yang mengandung properti atau metode tersebut dan
+            dalam objek turunannya.
+         4. Internal: Dapat diakses hanya dalam namespace atau assembly saat ini.
+         5. Protected Internal: Dapat diakses dalam namespace atau assembly saat ini dan dalam objek turunannya.
+
+         Jika kita membuat class tanpa access modifier dalam hal ini, access modifier default akan
+         digunakan, yaitu "internal". Ini berarti bahwa kelas tersebut hanya dapat diakses dalam
+         namespace atau assembly saat ini. Namun, disarankan untuk menentukan access modifier secara
+         eksplisit agar kode lebih jelas dan mudah dipahami.
+         */
     }
 }
